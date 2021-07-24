0291001819e850b2facffe36bb71737c09e970e4
L4rs <45175455+Hydr4bytes@users.noreply.github.com>
2021-07-24 20:55:30 +0200

    Discord Transport Layer????????

---
+++ Source/Networking/DiscordTransportLayer.cs
@@ -0,0 +1,78 @@
+using System;
+using Discord;
+
+namespace MultiplayerMod.Networking
+{
+    private static Discord.Discord discord;
+    
+    public class DiscordTransportConnection : ITransportConnection { 
+        public ulong ConnectedTo { get; private set; }
+        public bool IsConnected => IsValid;
+        internal bool IsValid { get; private set; } = true;
+
+        internal DiscordTransportConnection(ulong id, P2PMessage initialMessage)
+        {
+            var lobbyManager = discord.GetLobbyManager();
+            lobbyManager.ConnectNetwork(id);
+            // Reliable on 0, unreliable on 1
+            lobbyManager.OpenNetworkChannel(id, 0, true);
+            lobbyManager.OpenNetworkChannel(id, 1, false);
+            ConnectedTo = id;
+
+            SendMessage(initialMessage, SendReliability.Reliable);
+            MelonLogger.Msg($"Discord: Sent initial message to {id}");
+        }
+
+        public void Disconnect()
+        {
+            var lobbyManager = discord.GetLobbyManager();
+            lobbyManager.DisconnectLobby(lobbyManager.GetLobbyId(0));
+            IsValid = false;
+        }
+
+        public void SendMessage(P2PMessage msg, SendReliability sendType)
+        {
+            var lobbyManager = discord.GetLobbyManager();
+            lobbyManager.SendNetworkMessage(lobbyManager.GetLobbyId(0), ConnectedTo, sendType, msg.GetBytes());
+        }
+    }
+
+    public class DiscordTransportLayer : ITransportLayer {
+        public event Action<ITransportConnection, ConnectionClosedReason> OnConnectionClosed;
+        public event Action<ITransportConnection, P2PMessage> OnMessageReceived;
+        private readonly Dictionary<ulong, SteamTransportConnection> connections = new Dictionary<ulong, SteamTransportConnection>();
+
+        public ITransportConnection ConnectTo(ulong id, P2PMessage initialMessage)
+        {
+            if (connections.ContainsKey(id))
+            {
+                if (connections[id].IsValid)
+                    throw new ArgumentException("Already connected to " + id.ToString());
+                else
+                    connections.Remove(id);
+            }
+
+            MelonLogger.Msg($"Discord: Connecting to {id}");
+            DiscordTransportConnection connection = new DiscordTransportConnection(id, initialMessage);
+            connections.Add(id, connection);
+            SteamNetworking.OnP2PSessionRequest = ClientOnP2PSessionRequest;
+            SteamNetworking.OnP2PConnectionFailed = ClientOnP2PConnectionFailed;
+
+            return connection;
+        }
+
+        public void StartListening()
+        {
+            var lobbyManager = discord.GetLobbyManager();
+            lobbyManager.OnNetworkMessage = (lobbyId, userId, channelId, data) => {
+                OnMessageReceived.Invoke(connections[userId], new P2PMessage(data));
+            };
+        }
+
+        public void StopListening()
+        {
+            var lobbyManager = discord.GetLobbyManager();
+            lobbyManager.OnNetworkMessage = null;
+        }
+    }
+}
